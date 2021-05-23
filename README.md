e9668bcee9674ada1f4b9685e1acef8e1c60ac92
Victoria Sedano <vicki.sedano@gmail.com>
2021-05-22 23:47:52 -0500

    readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Car Maintenance
+
+AWS deploy <http://ec2-18-218-156-233.us-east-2.compute.amazonaws.com/>
+
+![](https://i.imgur.com/pwlaqsY.png)
+
+![](https://i.imgur.com/ByXcxK3.png)
+
--- index.ts
+++ index.ts
@@ -10,11 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
-//app.use(express.static(path.join(__dirname, '/client/build/index.html')));
 app.use('', routes);
-/*app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build/index.html'))
-});*/
 
 (async () => {
     try {
